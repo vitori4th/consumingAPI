42448b01249321ab05b784a12fe140a5545fc76b
Vitoria Thais <vitoriathdasilva@gmai.com>
2022-08-18 20:40:34 -0300

    consume in back-end

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express')
-const app = express()
 const cors = require('cors')
+const axios = require('axios')
 
+const app = express()
 app.use(cors())
 
-app.get('/', (req, res) => {
-  return res.json([{ name: 'Vitória' }, { name: 'Wesley' }])
+app.get('/', async (req, res) => {
+  // response é a resposta, mas eu tiro o data de dentro do response (response.data)
+  try {
+    const { data } = await axios('https://jsonplaceholder.typicode.com/users')
+    return res.json(data)
+  } catch (error) {
+    console.error(error)
+  }
 })
 app.listen('4567')
